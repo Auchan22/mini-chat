6c075aab4ab314942071e689d356e4aefe8486a7
Agustin <agustinsurila@gmail.com>
2023-01-05 01:07:42 -0300

    Arreglo en la scrollbar de mensajes

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,7 +6,7 @@ const Chat = () => {
   return (
     <div className=" relative  min-h-[80vh] w-full justify-end rounded-xl bg-slate-50 px-4 md:w-[40vw]">
       <div
-        className="message-list my-3 flex max-h-[calc(100vh-35vh)] min-h-[calc(100vh-35vh)] flex-col overflow-auto rounded-2xl bg-[#e5b3fe] pb-3 "
+        className="message-list my-3 flex max-h-[calc(100vh-35vh)] min-h-[calc(100vh-35vh)] flex-col overflow-auto rounded-2xl bg-[#e5b3fe] pr-2 pb-3 "
         style={{ boxShadow: "inset 0px -26px 40px -22px rgba(0,0,0,0.18)" }}
       >
         <Message />
